8507be4df561746e3435e659afe0855eb104f82e
nonari <bart.paczesny@gmail.com>
2023-04-13 20:12:02 +0200

    Adding some styles to list components

--- src/components/StepList.js
+++ src/components/StepList.js
@@ -1,9 +1,18 @@
+import style from "../styles/StepList.css";
 import React from "react";
+import StepListElement from "./StepListElement";
 
 function StepList(){
 
+    const textArr = ['your text', 'select plan', 'add-ons', 'summary'];
+
     return(
-        <div>list</div>
+        <div className="step-list">
+            <StepListElement number="1" text={textArr[0]}/>
+            <StepListElement number="2" text={textArr[1]}/>
+            <StepListElement number="3" text={textArr[2]}/>
+            <StepListElement number="4" text={textArr[3]}/>
+        </div>
     );
 };
 
--- src/components/StepListElement.js
+++ src/components/StepListElement.js
@@ -1,9 +1,18 @@
+import styles from "../styles/StepListElement.css"
 import React from "react";
 
-function StepListElement(){
+function StepListElement({number, text}){
 
     return(
-        <div>list element</div>
+        <div className="wrapper">
+            <div className="step-number">
+                <div>{number}</div>
+            </div>
+            <div>
+                <h2>Step {number}</h2>
+                <p>{text}</p>
+            </div>
+        </div>
     );
 };
 
